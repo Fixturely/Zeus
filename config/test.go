27cfa54dfe867f69879f3e3a8d2fcc2d911d7286
Manan Shah <manan@Manans-MacBook-Pro.local>
2025-09-17 23:59:17 -0700

    check db working

--- config/test.go
+++ config/test.go
@@ -2,10 +2,11 @@ package config
 
 func GetTestConfig(cfg *Config) {
 	cfg.Environment = "test"
-	cfg.Database.Host = "127.0.0.1"
-	cfg.Database.Port = 5441
-	cfg.Database.User = "admin_test"
-	cfg.Database.Password = "admin_test"
-	cfg.Database.Name = "zeus-test-db"
-	cfg.Database.SSLMode = false
+	// Allow overriding via environment variables; fall back to reasonable test defaults
+	cfg.Database.Host = getEnvOrDefault("DATABASE_HOST", cfg.Database.Host)
+	cfg.Database.Port = getEnvOrDefault("DATABASE_PORT", cfg.Database.Port)
+	cfg.Database.User = getEnvOrDefault("DATABASE_USER", cfg.Database.User)
+	cfg.Database.Password = getEnvOrDefault("DATABASE_PASSWORD", cfg.Database.Password)
+	cfg.Database.Name = getEnvOrDefault("DATABASE_NAME", cfg.Database.Name)
+	cfg.Database.SSLMode = getEnvOrDefault("DATABASE_SSL_MODE", false)
 }
