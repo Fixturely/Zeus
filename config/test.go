66ab4b07cf80607bbed3923498b7d2e1ca515454
Manan Shah <manan@Manans-MacBook-Pro.local>
2025-09-17 23:30:34 -0700

    try fixing db migrations for test

--- config/test.go
+++ config/test.go
@@ -2,7 +2,7 @@ package config
 
 func GetTestConfig(cfg *Config) {
 	cfg.Environment = "test"
-	cfg.Database.Host = "localhost"
+	cfg.Database.Host = "127.0.0.1"
 	cfg.Database.Port = 5441
 	cfg.Database.User = "admin_test"
 	cfg.Database.Password = "admin_test"
